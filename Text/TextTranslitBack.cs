2e752f87d4009815f3c5476574684c9c31119544
Vyacheslav <chelslava@gmail.com>
2026-03-22 22:01:03 +0300

    Remove unused translit imports

--- Text/TextTranslitBack.cs
+++ Text/TextTranslitBack.cs
@@ -26,8 +26,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using LTools.Common.Model;
 using LTools.Common.Model.Serialization;
 using LTools.Common.UIElements;
